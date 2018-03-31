17249194831bec4fcb5ef1f633723d66db8eeb74
Marcosvpj <marcos@marcosvpj.com.br>
2018-03-30 21:50:40 -0300

    Add icons and new screens

--- App.js
+++ App.js
@@ -8,9 +8,42 @@ import React, { Component } from 'react';
 
 import { TransactionsScreen } from './screens/TransactionsScreen';
 import { SummaryScreen } from './screens/SummaryScreen';
-import { TabNavigator } from 'react-navigation';
+import { CostsScreen } from './screens/CostsScreen';
+
+import { TabNavigator, TabBarBottom } from 'react-navigation';
+import Ionicons  from 'react-native-vector-icons/Ionicons';
 
 export default TabNavigator({
   'Resumo': { screen: SummaryScreen },
+  'Gastos': { screen: CostsScreen },
   'Transações': { screen: TransactionsScreen }
+}, {
+  initialRouteName: 'Resumo',
+  initialRouteParams: { name: 'teste param' },
+  tabBarPosition: 'bottom',
+  tabBarComponent: TabBarBottom,
+  tabBarOptions: {
+    // inactiveTintColor: 'gray',
+    style: {
+      elevation: .3,
+      backgroundColor: '#fff',
+      borderTopColor: '#fff'
+    }
+  },
+  navigationOptions: ({ navigation }) => ({
+    tabBarIcon: ({ focused, tintColor }) => {
+      // return <Ionicons name={'ios-card'} size={25} color={tintColor} />;
+      const { routeName } = navigation.state;
+      let iconName;
+      if (routeName === 'Resumo') {
+        iconName = `md-cash`;
+      } else if (routeName === 'Gastos') {
+        iconName = `md-apps`;
+      } else if (routeName === 'Transações') {
+        iconName = `md-card`;
+      }
+
+      return <Ionicons name={iconName} size={25} color={tintColor} />;
+    },
+  })
 });
